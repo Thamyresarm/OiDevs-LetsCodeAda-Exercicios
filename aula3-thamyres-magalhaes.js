08c5242b1e7f891be617595f072e711a2480f4b4
Thamyres Magalhães <pink.thamyres@gmail.com>
2022-09-02 20:04:10 -0300

    exercício 2

--- aula3-thamyres-magalhaes.js
+++ aula3-thamyres-magalhaes.js
@@ -1,4 +1,4 @@
-//Exeexicio 1
+//Exexicio 1
 
 const nomes = ["Thamyres","Deborah","Bia","Bruna","Tay","Jam"]
 
@@ -6,3 +6,12 @@ nomes.pop()
 
 console.log(nomes)
 
+// Exercicio 2
+
+notas = [7,5,10]
+
+somatorio = notas[0] + notas[1] + notas[2]
+
+media = somatorio / notas.length
+
+console.tableg(media)
