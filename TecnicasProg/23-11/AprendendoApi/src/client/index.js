c7c30c6e5ef8f65665636fb8d578220f4e787a35
Thamyresarm <pink.thamyres@gmail.com>
2022-11-23 22:06:02 -0300

    segunda parte da aula dia 23

--- TecnicasProg/23-11/AprendendoApi/src/client/index.js
+++ TecnicasProg/23-11/AprendendoApi/src/client/index.js
@@ -1,15 +1,43 @@
-http.get("http://localhost:3000/", (res) => {
-    let data = '';
+import http from 'http'
+import  fs  from "fs"
 
-    // Um bloco de dados foi recebido.
-    res.on('data', (chunk) => {
-      data += chunk;
-    });
-  
-    // Toda a resposta foi recebida. Exibir o resultado.
+http.get('http://localhost:3000/', (res) => {
+    const { statusCode } = res;
+    const contentType = res.headers['content-type'];
+    let error;
+    // Any 2xx status code signals a successful response but
+    // here we're only checking for 200.
+    if (statusCode !== 200) {
+        error = new Error('Request Failed.\n' +
+            `Status Code: ${statusCode}`);
+    } else if (!/^application\/json/.test(contentType)) {
+        error = new Error('Invalid content-type.\n' +
+            `Expected application/json but received ${contentType}`);
+    }
+    // exibição do erro
+    if (error) {
+        console.error(error.message);
+        // Consume response data to free up memory
+        res.resume();
+        return;
+    }
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', (chunk) => { rawData += chunk; });
     res.on('end', () => {
-      console.log(JSON.parse(data));
+        try {
+            const parsedData = JSON.parse(rawData);
+            fs.writeFile('apirecebida.txt', JSON.stringify(parsedData), (err) => {
+                if (err) {
+                    console.log('erro');
+                } else {
+                    console.log('Arquivo salvo com sucesso\n');
+                }
+            })
+        } catch (e) {
+            console.error(e.message);
+        }
     });
-})
-
-export {}
+}).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
+});
