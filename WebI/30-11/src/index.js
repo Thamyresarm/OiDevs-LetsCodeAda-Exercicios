d5c16aa9ed37f32a71830159a52bf7770563c0a8
Thamyresarm <pink.thamyres@gmail.com>
2022-11-30 22:12:22 -0300

    aula 30-11 finalizada

--- WebI/30-11/src/index.js
+++ WebI/30-11/src/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 
 const server = express();
+
+server.use(express.json());
+
 const port = 3333;
+let users = [];
 
 // function getRoot(request, response) {
 //     response.send('Hello World!');
@@ -10,11 +14,42 @@ const port = 3333;
 // server.use('/', getRoot);
 
 server.get('/', (request, response) => {
-    response.send('Hello World from get!');
+    response.send(users);
+    // response.send('Hello World from get!');
 });
 
 server.post('/', (request, response) => {
-    response.send('Hello World from post!');
+    const user = request.body;
+    users.push(user);
+    console.log(request.body);
+
+    response.send(users);
+    // response.send('Hello World from post!');
+});
+
+server.put('/:id', (request, response) => {
+    const {id} = request.params;
+    const userIndex = users.findIndex(x => x.id === Number(id));
+
+    if(userIndex ===  -1){
+        //nao encontrou
+       return response.send('Not Found!');
+        
+     }
+        users[userIndex].name = request.body.name;
+        users[userIndex].data_nascimento = request.body.data_nascimento;
+
+        response.send(users[userIndex]);
+
+    // if(userIndex ===  -1){
+    //     //nao encontrou
+    //     response.send('Not Found!');
+    //  } else {
+    //     users[userIndex].name = request.body.name;
+    //     users[userIndex].data_nascimento = request.body.data_nascimento;
+
+    //     response.send(users[userIndex]);
+    // }
 });
 
 server.listen(port, () => {
